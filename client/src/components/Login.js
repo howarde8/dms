fa83ec5566ca24d5f5eaa69b105b43db7b82f914
Howard <howarde8@gmail.com>
2019-06-28 11:33:21 +0700

    Add Login page

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,49 @@
 import React, { Component } from "react";
+import { Form, Icon, Input, Button } from "antd";
 
 class Login extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log(values);
+      }
+    });
+  };
+
   render() {
+    const { getFieldDecorator } = this.props.form;
     return (
-      <div>Login</div>
+      <Form onSubmit={this.handleSubmit} style={{ maxWidth: "300px" }}>
+        <Form.Item>
+          {getFieldDecorator("username", {
+            rules: [{ required: true, message: "Please input username" }]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Username"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("password", {
+            rules: [{ required: true, message: "Please input password" }]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+            Log in
+          </Button>
+        </Form.Item>
+      </Form>
     );
   }
 }
 
-export default Login;
+export default Form.create()(Login);
