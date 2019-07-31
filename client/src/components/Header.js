20b9179f03e743fa96c3665a108e624a52d300a8
Howard <howarde8@gmail.com>
2019-07-31 16:19:45 +0800

    Header and Login UI improvement

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,70 @@
 import React, { Component } from "react";
-import { Button } from "antd";
+import { Link } from "react-router-dom";
+import { Button, Layout, Menu, Spin, Typography } from "antd";
 
 class Header extends Component {
+  renderUser = () => {
+    switch (this.props.auth.user) {
+      case undefined:
+      case null:
+        return <Spin />;
+      case false:
+        return <div />;
+      default:
+        return (
+          <div>
+            <Button ghost type="link" icon="user">
+              {this.props.auth.user.username}
+            </Button>
+            <Button
+              ghost
+              type="link"
+              icon="logout"
+              onClick={this.props.onLogoutClick}
+            >
+              Logout
+            </Button>
+          </div>
+        );
+    }
+  };
+
   render() {
     return (
-      <div>
-        <div>Header</div>
-        <div>
-          <p>User: {this.props.auth.user.username}</p>
-          <Button onClick={this.props.onLogoutClick}>Logout</Button>
-        </div>
-      </div>
+      <Layout.Header>
+        <Link to="/">
+          <Typography
+            style={{
+              float: "left",
+              marginRight: "40px",
+              color: "white",
+              fontSize: "24px"
+            }}
+          >
+            DMS
+          </Typography>
+        </Link>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          selectable={false}
+          style={{ float: "left", lineHeight: "64px" }}
+        >
+          <Menu.Item key="user">
+            <Link to="/user">User</Link>
+          </Menu.Item>
+          <Menu.Item key="level">
+            <Link to="/level">Level</Link>
+          </Menu.Item>
+        </Menu>
+        <Typography
+          style={{
+            float: "right"
+          }}
+        >
+          {this.renderUser()}
+        </Typography>
+      </Layout.Header>
     );
   }
 }
